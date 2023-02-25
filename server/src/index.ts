f8f12477e27cddb186888ca0a925b4041fe3b7c0
aman-zaidi01 <91572410+aman-zaidi01@users.noreply.github.com>
2023-02-25 17:18:34 -0500

    Configuration Fix | AZ

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,21 +1,21 @@
-import express from 'express';
-import { graphqlHTTP } from 'express-graphql';
-import cors from 'cors';
-import { createConnection } from 'typeorm';
+import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import cors from "cors";
+import { createConnection } from "typeorm";
 
 const main = async () => {
-        const app = express();
-        app.use(cors());
-        app.use(express);
-        // app.use("/graphql",graphqlHTTP({
-        //         schema,
-        //         graphiql: true
-        // }))
-        app.listen(3001, () => {
-                console.log("Server running at port 3001");
-        })
-}
+  const app = express();
+  app.use(cors());
+  app.use(express);
+  // app.use("/graphql",graphqlHTTP({
+  //         schema,
+  //         graphiql: true
+  // }))
+  app.listen(3001, () => {
+    console.log("Server running at port 3001");
+  });
+};
 
 main().catch((err) => {
-        console.error(err);
-})
+  console.error(err);
+});
